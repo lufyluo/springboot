41733bdfdf610fcb3bf7e16758ed033567d81b0e
luofei <lufyluo@163.com>
2019-04-10 17:30:06 +0800

    1、修改多key查询

--- redis/src/main/java/com/example/redis/controller/RedisRestController.java
+++ redis/src/main/java/com/example/redis/controller/RedisRestController.java
@@ -4,6 +4,9 @@ import com.example.redis.infrastructure.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * @Desc : .....
  * @Author : lufy
@@ -18,6 +21,12 @@ public class RedisRestController {
     public String get(@RequestParam String key){
         return redisUtil.get(key);
     }
+
+    @RequestMapping(value = "load/{key}",method = RequestMethod.GET)
+    public List<String> load(@PathVariable String key){
+        return redisUtil.mget(key.split(","));
+    }
+
     @RequestMapping(value = "set/{key}/{value}",method = RequestMethod.GET)
     public String set(@PathVariable String key, @PathVariable  String value){
         return redisUtil.set(key,value);
